e321aa7ce3b21d970450b9e03ec75d54a34b43ad
mariley <mariley2018@gmail.com>
2021-07-07 21:18:16 -0500

    add AppBar component and action logout

---
+++ src/components/AppBar/AppBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      El rey
+    </div>
+
+    <v-spacer />
+
+    <v-btn
+      text
+      @click="logout"
+    >
+      <v-icon>mdi-logout</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  methods: {
+    ...mapActions({
+      logout: 'auth/logout'
+    }),
+    name () {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
